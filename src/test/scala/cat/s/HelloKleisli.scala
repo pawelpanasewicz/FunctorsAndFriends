8004515b6a9ef248313e503650389ed2ddc8309a
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-09-15 15:46:58 +0200

    hello Kleisli

---
+++ src/test/scala/cat/s/HelloKleisli.scala
@@ -0,0 +1,36 @@
+package cat.s
+
+import cats.FlatMap
+import cats.data.Kleisli
+
+
+class HelloKleisli extends ynfrastructure.Spec {
+
+  "hello kleisli" in {
+
+    //given functions in type signare: A => F[B] and B => F[C]
+    //you may want to create 3rd function A => F[C]
+
+    val fa: String => Option[Int] = s => if(s.isEmpty) None else Some(s.size)
+    val fb: Int => Option[Double] = i => if(i == 0) None else Some(102.0/(i.toDouble))
+    val fc: String => Option[Double] = s => fa(s).flatMap(fb)
+
+    fc("abc") mustBe Some(102.0/3.0)
+    fc("") mustBe None
+
+    //or you can wrap values inside kleisli:
+
+    //needed by Kleisli
+    implicit val faltMapOption: FlatMap[Option] = cats.instances.option.catsStdInstancesForOption
+
+    val faK = Kleisli[Option, String, Int](fa)
+    val fbK = Kleisli[Option, Int, Double](fb)
+    val fcK = faK.andThen(fbK)
+
+    fcK.run("abc") mustBe Some(102.0/3.0)
+    fcK.run("") mustBe None
+
+
+  }
+
+}
