0d0dd1363d878ad0bcbae850cfc8466b7d1706f1
pawel.panasewicz <pawel.panasewicz@gmail.com>
2016-09-16 14:55:35 +0200

    yet another behaviour in typeclasses demo

--- src/test/scala/simulacru/m/TypeclassDemo.scala
+++ src/test/scala/simulacru/m/TypeclassDemo.scala
@@ -3,14 +3,23 @@ package simulacru.m
 
 import simulacrum._
 
+//This is just a behaviour, some type will have it and other won't
 @typeclass trait CanTruthy[A] {
   def truthy(a: A): Boolean
+}
+
+//this is just another behaviour, some type will have it, others won't
+@typeclass trait MakeBigger[A] {
+  /**
+    * @return something bigger than 'a'
+    */
+  def makeBigger(a: A): A
 }
 
 class TypeclassDemo extends ynfrastructure.Spec {
 
   "stringTruthy" in {
-    implicit val x = monoidInstances.stringTruthy
+    implicit val x = TypeClassInstances.string.stringTruthy
     CanTruthy[String] truthy "" mustBe true
     CanTruthy[String].truthy("asdf") mustBe false
 
@@ -20,7 +29,7 @@ class TypeclassDemo extends ynfrastructure.Spec {
   }
 
   "stringTruthy2" in {
-    implicit val x = monoidInstances.yesStringTruthy
+    implicit val x = TypeClassInstances.string.yesStringTruthy
     CanTruthy[String] truthy "" mustBe false
     CanTruthy[String].truthy("asdf") mustBe false
     CanTruthy[String].truthy("yes") mustBe true
@@ -31,17 +40,35 @@ class TypeclassDemo extends ynfrastructure.Spec {
     "siala baba mak".truthy mustBe false
   }
 
-  object monoidInstances {
+  "string can be made bigger" in {
+    import TypeClassInstances.string.stringMakeBigger
+    import MakeBigger.ops._
 
+    "I am the small string".makeBigger mustBe "I AM THE BIGGER MUCH MUCH MUCH BIGGER STRING AAAAA"
+  }
+
+
+  object TypeClassInstances {
+
+    object string {
+      implicit val stringTruthy: CanTruthy[String] = ct(_.isEmpty)
+      implicit val yesStringTruthy: CanTruthy[String] = ct(_.toLowerCase() == "yes")
+      implicit val stringMakeBigger: MakeBigger[String] = bigger(_.toUpperCase.replaceAllLiterally("SMALL", "BIGGER MUCH MUCH MUCH BIGGER") + " AAAAA")
+    }
+
+    object int {
+      implicit val intTruthy: CanTruthy[Int] = ct(_ == 0)
+      implicit val intTruthy2: CanTruthy[Int] = ct(_ >= 0)
+    }
+
+    //convinience method for creating CanTruthy
     def ct[A](f: A => Boolean): CanTruthy[A] = new CanTruthy[A] {
       override def truthy(a: A): Boolean = f(a)
     }
-
-    implicit val stringTruthy: CanTruthy[String] = ct(_.isEmpty)
-    implicit val yesStringTruthy: CanTruthy[String] = ct(_.toLowerCase() == "yes")
-    implicit val intTruthy: CanTruthy[Int] = ct(_ == 0)
-    implicit val intTruthy2: CanTruthy[Int] = ct(_ >= 0)
+    //convinience method for creating MakeBigger
+    def bigger[A](f: A => A): MakeBigger[A] = new MakeBigger[A] {
+      override def makeBigger(a: A): A = f(a)
+    }
   }
-
 }
 
